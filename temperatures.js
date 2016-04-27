2e9f77be2d1cc2e54745739745431aafc2aef352
Maxime Bernard <mbernard@wemanity.com>
2016-04-27 16:21:01 +0200

    add another challenge

---
+++ temperatures.js
@@ -0,0 +1,28 @@
+function getHighestAndLowest(temperatures) {
+    // Your code here
+    var lowestTemp = temperatures[0];
+    var highestTemp = temperatures[temperatures.length - 1];
+
+    return formatMyResult(lowestTemp.value, highestTemp.value);
+}
+
+/** DO NOT TOUCH THIS */
+
+function formatMyResult(min, max) {
+    return 'Les températures se situent entre ' + min + '°C et ' + max + '°C';
+}
+
+var request = require("request");
+
+request({
+    url: 'http://ns323481.ip-37-187-0.eu/coding/temperature10000.json',
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        console.time('temperatures'); 
+        var result = getHighestAndLowest(response.body);
+        console.timeEnd('temperatures');
+        console.log('Result: ' + result);
+    }
+})
