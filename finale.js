f23410f70213116891e89875ebc612ecfcbb8295
Maxime Bernard <mbernard@wemanity.com>
2016-04-27 15:11:02 +0200

    Final challenge for coding battle

---
+++ finale.js
@@ -0,0 +1,28 @@
+function getMeTheLowestPrice(clubs) {
+    // Your code here
+    var lowestClub = clubs[0];
+    var lowestSeason = lowestClub.seasons[0];
+    
+    return formatMyResult(lowestClub.title, lowestSeason.data.country, lowestSeason.data.price, lowestSeason.title);
+}
+
+/** DO NOT TOUCH THIS */
+
+function formatMyResult(clubName, clubCountry, clubPrice, season) {
+    return clubName + ' en ' + clubCountry + ' à partir de ' + clubPrice + ' pour la saison ' + season;
+}
+
+var request = require("request");
+
+request({
+    url: 'http://static.front.cmb-uk.kyokita.net/resorts/en_resorts.json',
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        console.time('getMeTheLowestPrice'); 
+        var result = getMeTheLowestPrice(response.body);
+        console.timeEnd('getMeTheLowestPrice');
+        console.log('Result: ' + result);
+    }
+})
